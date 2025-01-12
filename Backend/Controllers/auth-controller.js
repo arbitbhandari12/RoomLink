9fe9a4f2a5b5c23c0645180caeda3859e8adebb3
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-12 19:24:41 +0545

    Create auth-controller.js

---
+++ Backend/Controllers/auth-controller.js
@@ -0,0 +1,71 @@
+const User = require('../models/user-model');
+const bcrypt = require('bcrypt'); // Import bcrypt for password hashing
+// const approve = require('../models/propertyapprove-model');
+
+const home = async (req, res) => {
+  try {
+    res.send('This is home page using router');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Register User
+
+const register = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+    console.log(req.body); // Log the incoming request to see what's being sent
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log('Email already exists');
+      return res.status(400).json({ msg: 'Email already exists' });
+    }
+
+    const saltRounds = 10;
+    const hash_password = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await User.create({
+      username,
+      email,
+      phone,
+      password: hash_password
+    });
+    res.status(201).json({
+      msg: 'User registered successfully',
+      token: await newUser.generateToken(),
+      userId: newUser._id.toString()
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error. Please try again later.' });
+  }
+};
+
+//Completed Register User
+
+//Login User
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(400).json({ msg: 'Do register first' });
+    }
+    const user = await bcrypt.compare(password, existingUser.password);
+    if (user) {
+      res.status(200).json({
+        msg: 'User Login successfully',
+        token: await existingUser.generateToken(),
+        userId: existingUser._id.toString()
+      });
+    } else {
+      res.status(401).json({ msg: 'Password not valid.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error. Please try again later.' });
+  }
+};
