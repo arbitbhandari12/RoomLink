8545955bc65f774a99cf5e9ce743d95a6d238649
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-20 22:13:51 +0545

    Create auth-router.js

---
+++ Backend/router/auth-router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {
+  home,
+  register,
+  login,
+  user,
+} = require('../controllers/auth-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+
+router.route('/').get(home);
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/user').get(authMiddleware, user);
+
+module.exports = router;
