63d3ebf6bcd5aff8545abb3eb82ed66f51d590b9
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-20 22:13:38 +0545

    Create admin-router.js

---
+++ Backend/router/admin-router.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const {
+  allUsers,
+  deleteUsers,
+  allproperty,
+  detailsPage,
+  deleteProperty,
+  approveProperty,
+} = require('../controllers/admin-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+const adminMiddleware = require('../middlewares/admin-middleware');
+
+router.route('/admin').get(authMiddleware, adminMiddleware);
+router.route('/users').get(authMiddleware, adminMiddleware, allUsers);
+router.route('/property').get(authMiddleware, adminMiddleware, allproperty);
+router.route('/properties/:id').get(detailsPage);
+
+router
+  .route('/users/delete/:id')
+  .delete(authMiddleware, adminMiddleware, deleteUsers);
+
+router
+  .route('/property/delete/:id')
+  .delete(authMiddleware, adminMiddleware, deleteProperty);
+
+router
+  .route('/property/approve/:id')
+  .get(authMiddleware, adminMiddleware, approveProperty);
+
+module.exports = router;
