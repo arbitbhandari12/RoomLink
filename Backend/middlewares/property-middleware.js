90c208fc5129cbfbe3f6223a227f9e74fb01b9a9
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-25 15:31:13 +0545

    Create property-middleware.js

---
+++ Backend/middlewares/property-middleware.js
@@ -0,0 +1,11 @@
+// const List = require('../models/propertyList-model');
+
+// const allProperty = async (req, res) => {
+//   try {
+//     const all
+//   } catch (error) {
+//     res.status(500).json({ error: 'Server error. Please try again later.' });
+//   }
+// };
+
+// module.exports = allProperty;
