05b4cda6122c558d621b162cdb1ee5ecf60d6c4f
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-25 16:44:02 +0545

    dependency

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient-right-top': 'linear-gradient(to right top, #ff7f50, #c1862e, #868430, #567941, #356950, #396e64, #497173, #5d747a, #829199, #a8afb6, #cdced3, #f0f0f0)',
+      },
+    },
   },
   plugins: [],
 }
