c45a0d5aec3bf797d96f7e31c3c1cabc943fa893
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-27 20:11:47 +0545

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,41 +1,72 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './Components/Header';
 import Home from './Pages/Home';
-import PropertyAvailability from './Pages/PropertyAvailibility';
-import PropertyDetails from './Pages/DetailsPage';
-import AddProperty from './Pages/AddProperty';
-import RoomShifting from './Pages/RoomShifting';
 import Login from './Pages/Login';
-import Signup from './Pages/Register';
-import Header from './components/header';
-import Footer from './components/footer';
-import AboutUs from './Pages/AboutUs';
-import AdminHome from './Pages/AdminHome';
+import Register from './Pages/Register';
+import PropertyAvailability from './Pages/PropertyAvailability';
+import Logout from './Pages/Logout';
+import AdminLayout from './Components/Layouts/Admin-Layout';
 import AdminUser from './Pages/AdminUser';
-import AdminlistProperty from './Pages/AdminListProperty';
-import AdminShifting from './Pages/AdminShift';
-import TenantDetails from './Pages/AdminShftingdetails';
+import AdminListProperty from './Pages/AdminListProperty';
+import AddProperty from './Pages/AddProperty';
+import PropertyDetails from './Pages/SingleProperty';
+import UserProperty from './Pages/UserProperty';
+import EditProperty from './Pages/EditProperty';
+import PropertyLayout from './Components/Layouts/Property-Layout';
+import Footer from './Components/Footer';
+import RoomShifting from './Pages/RoomShifting';
+import DetailsPropertyAdmin from './Pages/DetailsPropertyAdmin';
+import ForgotPassword from './Pages/ForgotPassword';
+import PersonalPropertyDetails from './Pages/PersonalPropertyDetails';
+import AdminShift from './Pages/AdminShift';
+import AdminShifting from './Pages/AdminShifting';
+import AdminHome from './Pages/AdminHome';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
+  const location = useLocation();
+
+  // Check if the current path starts with '/admin'
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/PropertyAvailability"
+          element={<PropertyAvailability />}
+        />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/properties/:id" element={<DetailsPropertyAdmin />} />
+        <Route path="/yourproperty/:id" element={<PersonalPropertyDetails />} />
+        <Route path="editProperty/:id" element={<EditProperty />}></Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/availableRooms" element={<PropertyAvailability />} />
-        <Route path="/details" element={<PropertyDetails />} />
-        <Route path="/addProperty" element={<AddProperty />} />
-        <Route path="/roomShifting" element={<RoomShifting />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/addproperty" element={<PropertyLayout />}>
+          <Route index element={<AddProperty />} />{' '}
+          <Route path="UserProperty" element={<UserProperty />}></Route>
+        </Route>
         <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/admin" element={<AdminHome />} />
-        <Route path="/adminUser" element={<AdminUser />} />
-        <Route path="/adminListProperty" element={<AdminlistProperty />} />
-        <Route path="/adminShift" element={<AdminShifting />} />
-        <Route path="/adminShiftDetails" element={<TenantDetails />} />
+        <Route path="/Roomshifting" element={<RoomShifting />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="home" element={<AdminHome />} />
+          <Route path="users" element={<AdminUser />} />
+          <Route path="listofproperty" element={<AdminListProperty />} />
+          <Route path="properties/:id" element={<DetailsPropertyAdmin />} />
+          {/* <Route path="complaint-property" element={<AdminComplaint />} /> */}
+          <Route path="shifting" element={<AdminShift />} />
+          <Route
+            path="adminShifting"
+            element={<AdminShifting />}
+          />
+        </Route>
       </Routes>
-      <Footer />
-    </Router>
+      {!isAdminRoute && <Footer />}
+    </>
   );
 }
 
