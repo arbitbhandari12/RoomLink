64992cf6838bf739457e82d2d9db9bee69fa7c3d
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-28 21:09:09 +0545

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './Store/auth.jsx';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+      <ToastContainer />
+    </AuthProvider>
+  </BrowserRouter>
 );
