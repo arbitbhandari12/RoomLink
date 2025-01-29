66c01a730b102985cc5f28b7106cc96398a85bf0
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-29 23:18:16 +0545

    Update property-router.js

--- Backend/router/property-router.js
+++ Backend/router/property-router.js
@@ -12,7 +12,7 @@ const {
   homeproperty
 } = require('../controllers/userSideProperty-controller');
 const updateProperty = require('../Controllers/editProperty-controller');
-const similarProperty = require("../Controllers/SimilarProperty-controller")
+const similarProperty = require('../Controllers/SimilarProperty-controller');
 const booking = require('../controllers/BookingProperty-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
 
@@ -22,7 +22,9 @@ router.route('/available').get(userSideProperty);
 router.route('/property/:id').get(propertyPage);
 router.route('/myProperty').get(authMiddleware, personalProperty);
 router.route('/yourProperties/:id').get(yourProperties);
-router.route('/update/:id').patch(authMiddleware, upload.array('photos', 10), updateProperty);
+router
+  .route('/update/:id')
+  .patch(authMiddleware, upload.array('photos', 10), updateProperty);
 router.route('/similar/:id').get(authMiddleware, similarProperty);
 router.route('/delete/:id').delete(authMiddleware, deleteProperty);
 router.route('/editProperty/:id').get(authMiddleware, editRoom);
