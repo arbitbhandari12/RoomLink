6702f6217103a52bd9b041220b124a7325dd6e9d
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-01-29 20:48:00 +0545

    Update property-router.js

--- Backend/router/property-router.js
+++ Backend/router/property-router.js
@@ -11,8 +11,9 @@ const {
   editRoom,
   homeproperty
 } = require('../controllers/userSideProperty-controller');
+const updateProperty = require('../Controllers/editProperty-controller');
 const booking = require('../controllers/BookingProperty-controller');
-const authMiddleware = require('../middlewares/auth-middleware'); //editRoom
+const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post('/addproperty', upload.array('photos', 10), addProperty);
 router.route('/homelatest').get(homeproperty);
@@ -20,6 +21,7 @@ router.route('/available').get(userSideProperty);
 router.route('/property/:id').get(propertyPage);
 router.route('/myProperty').get(authMiddleware, personalProperty);
 router.route('/yourProperties/:id').get(yourProperties);
+router.route('/update/:id').patch(authMiddleware, upload.array('photos', 10), updateProperty);
 router.route('/delete/:id').delete(authMiddleware, deleteProperty);
 router.route('/editProperty/:id').get(authMiddleware, editRoom);
 router.route('/booking').post(authMiddleware, booking);
