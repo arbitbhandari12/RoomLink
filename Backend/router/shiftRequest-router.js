f663a1673f482ea8b3bd7ab462ebe8f3fae9d4fd
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-02-07 22:01:52 +0545

    Delete shiftRequest-router.js

--- Backend/router/shiftRequest-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shiftRoom = require('../Controllers/shiftingRequest-controller');
-
-router.route('/shiftRequest').post(shiftRoom);
-
-module.exports = router;
