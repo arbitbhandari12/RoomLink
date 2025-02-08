84ce84aaf4a545fbfda4c3a6cf8a3939f6866385
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-02-08 20:32:06 +0545

    changes on shifting

--- Backend/router/Shifting-router.js
+++ Backend/router/Shifting-router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const shiftRoom = require('../Controllers/shiftingRequest-controller');
-const adminShift = require('../Controllers/adminShift-controller');
+const { shiftingRequest, details } = require('../Controllers/adminShift-controller');
 
 router.route('/shiftRequest').post(shiftRoom);
-router.route('/adminShift').get(adminShift);
+router.route('/adminShift').get(shiftingRequest);
+router.route('/shiftDetails/:id').get(details);
 
 module.exports = router;
