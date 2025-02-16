577edd1189704237bc5b745865a82001f707c904
Arbit Bhandari <152582498+arbit69@users.noreply.github.com>
2025-02-16 22:31:56 +0545

    Update editProperty-controller.js

--- Backend/Controllers/editProperty-controller.js
+++ Backend/Controllers/editProperty-controller.js
@@ -2,7 +2,17 @@ const propertyList = require('../models/propertyList-model');
 
 const updateProperty = async (req, res) => {
   try {
+    const user = req.user;
+    const isAdmin = user.isAdmin;
     const id = req.params.id;
+
+    let status = '';
+
+    if (isAdmin == true) {
+      status = 'Approved';
+    } else {
+      status = 'Pending';
+    }
 
     const updatedData = {
       title: req.body.title,
@@ -26,7 +36,7 @@ const updateProperty = async (req, res) => {
       name: req.body.name,
       phone: req.body.phone,
       email: req.body.email,
-      status:"Pending"
+      status: status
     };
 
     if (req.files && req.files.length > 0) {
@@ -41,16 +51,13 @@ const updateProperty = async (req, res) => {
     );
 
     if (!updatedProperty) {
-      return res
-        .status(404)
-        .json({ message: 'Property not found.' });
+      return res.status(404).json({ message: 'Property not found.' });
     }
 
     res.status(200).json({
       message: 'Property updated successfully!',
       property: updatedProperty
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Failed to update property.' });
